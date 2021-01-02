349ed750b352429a2d7451a4042bad910280ab7c
Harsh Jha <HARSHJHASD@users.noreply.github.com>
2021-01-02 15:34:11 +0530

    final commit

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-//You will build this project using pure Node to gain a thorough understanding of Node. There are obviously easier ways of building this project using external modules, but that is the purpose of this project as you would miss out on many code concepts.
+//You will build this project using pure Node to gain a thorough understanding of Node.
+// There are obviously easier ways of building this project using external modules,
+ // but that is the purpose of this project as you would miss out on many code concepts.
+
 
 
 //require node modules
 const http = require('http');
 
-//file imports
+//file imports for importing modules from respond.js located at same level
 const respond = require('./lib/respond.js');
 
-//connection settings
+//connection settings ,we have used process.env.port for the time we wil be deployying our project on heroku
 const port = process.env.PORT || 3000;
 
 //Create server
 const server = http.createServer(respond);
 
+
 //listen to client requests on the specific port, the port should be available
-server.listen(port, () => {
+  server.listen(port, () => {
     console.log(`listening on port: ${port}`);
 });
--- playground/path.js
+++ playground/path.js
@@ -1,7 +1,10 @@
+// requiring the path module
 const path = require('path');
 
 console.log(path.join('Node', 'Project1'));
 
 console.log(path.normalize('../../Node/../Built in Modules fs'));
 
+
+//  
 console.log(path.resolve('../../Node/../Built in Modules fs'));
